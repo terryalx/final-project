54435e0760b02227908188824e8ad0192f304173
Efe-Terry <info.techbro.set@gmail.com>
2024-07-09 14:17:43 +0100

    fixed get

--- blog01/models.py
+++ blog01/models.py
@@ -8,7 +8,7 @@ from PIL import Image
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.CharField(max_length=250, default="No description available")
+    post = models.CharField(max_length=2000, default="No description available")
     image = models.ImageField(upload_to="blogPostImages/%Y/%m/%d")
     tag = models.CharField(max_length=10)
     date = models.DateTimeField(default=timezone.now)
